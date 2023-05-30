9307f73e715bd37c61039cee284f4840f0994fff
kneckebrodet <113566464+kneckebrodet@users.noreply.github.com>
2023-05-31 07:31:38 +0900

    Update morning.py

--- morning.py
+++ morning.py
@@ -2,7 +2,7 @@ from database import MongoDB
 from datetime import datetime
 
 class Morning:
-    def __init__(self):
+    def __init__(self, id):
         # get date and time
         self.today = str(datetime.now().date())
         self.time = str(datetime.now().time().strftime("%H:%M"))
@@ -12,9 +12,10 @@ class Morning:
         self.db.connect_to_localhost()
 
         # add wake up time to db
-        wutime_data = {"date":self.today, "bedtime": self.time}
-        if not self.db.wutime_collection.find_one({"date":self.today}):
+        wutime_data = {"userID":id, "date":self.today, "time": self.time}
+        if not self.db.wutime_collection.find_one({"userID":id, "date":self.today}):
             self.db.add_data("wutime_collection", wutime_data)
 
+        self.todays_wakeup_time = self.db.get_wakeup_time(id)
 
 
