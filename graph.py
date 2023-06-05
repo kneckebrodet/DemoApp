782f04e5e442fd05f076e69b8f52c075f39bbbab
kneckebrodet <113566464+kneckebrodet@users.noreply.github.com>
2023-06-06 06:59:26 +0900

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,45 +1,41 @@
-from kivy.uix.image import CoreImage
-import io
+
+from database import MySQL
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 import matplotlib.pyplot as plt
 import numpy as np
-from database import MongoDB
 
 class Graph:
     def __init__(self, app, id, dates, targets):
-        self.db = MongoDB()
-        self.db.connect_to_localhost()
         self.app = app
         self.id = id
         self.dates = dates
         self.targets = targets
-        self.active_dates = []
+        self.recorded_dates = []
 
         self.create_first_graph()
-
-        if not self.targets:
-            self.app.root.get_screen("graph").ids.img2.texture = None
-        else:
+        if self.targets:
             self.create_second_graph()
 
+
+    ### FIRST GRAPH ( WEIGHT SLEEP )
     def create_first_graph(self):
-        plt.clf()
         ## Remove dates without stats and create two lists: [active dates], [[date_stats], x4]
         all_records = []
+        self.db = MySQL()
         for date in self.dates:
             self.date_has_obj = False
             date_stats = []
             try:
-                date_stats.append(self.db.data_collection.find_one({"userID":self.id, "date":date})["sleep"])
-                date_stats.append(self.db.data_collection.find_one({"userID":self.id, "date":date})["weight"])
+                date_stats.append(self.db.get_stat(self.id, date, "sleep"))
+                date_stats.append(self.db.get_stat(self.id, date, "weight"))
                 #date_stats.append(db.data_collection.find_one({"userID":id, "date":date})["meditaion"])
                 #date_stats.append(db.data_collection.find_one({"userID":id, "date":date})["ifast"])
                 self.date_has_obj = True
             except:
                 continue
             if self.date_has_obj:
-                self.active_dates.append(date)
+                self.recorded_dates.append(date)
                 all_records.append(date_stats)
-
 
         ## Check if data in database
         if len(all_records) < 2:
@@ -62,43 +58,40 @@ class Graph:
             ## Create the Graph
             plt.clf()
 
+            fig, ax = plt.subplots()
+            canvas = fig.canvas
+
             ## change the format of the date (x axis) from : 2023-05-29 => 23/05/29 for cleaner look
             x = []
-            for index, date in enumerate(self.active_dates):
-                x.append(str(self.active_dates[index]).replace("-","/")[2:])
+            for index, date in enumerate(self.recorded_dates):
+                x.append(str(self.recorded_dates[index]).replace("-","/")[2:])
 
             for y in records_by_cat:
-                plt.plot(x, y)
+                ax.plot(x,y)
 
             plt.legend(["Sleep", "Weight"])
-            plt.title("")
-
             plt.ylim(0, 15)
-
-            if len(self.active_dates) > 10:
-                plt.xticks(np.arange(0, len(self.active_dates), len(self.active_dates)//10))
-
+            if len(self.recorded_dates) > 10:
+                plt.xticks(np.arange(0, len(self.recorded_dates), len(self.recorded_dates)//10))
+            
             plt.xticks(rotation='vertical')
             plt.tight_layout()
 
-            buf = io.BytesIO()
-            plt.savefig(buf)
+            canvas = FigureCanvasKivyAgg(plt.gcf())
+            self.app.root.get_screen("graph").ids.bx1.add_widget(canvas)
 
-            buf.seek(0)
-            self.app.root.get_screen("graph").ids.img1.texture = CoreImage(buf, ext="png").texture
-            del buf
-
+            
+    ### SECOND GRAPH ( CHECKED BOXES )
     def create_second_graph(self):
         ## get all the targets and dates
-        plt.clf()
         all_records = []
         dates = []
-        for date in self.active_dates:
+        for date in self.recorded_dates:
             stats = []
             self.date_has_obj = False
             for target in self.targets:
                 try:
-                    stats.append(self.db.data_collection.find_one({"userID":self.id, "date":date})[target])
+                    stats.append(self.db.get_stat(self.id,date, target))
                     self.date_has_obj = True
                 except:
                     continue
@@ -108,18 +101,21 @@ class Graph:
 
         ## Sort the stats by category instead of date
         records_by_cat = [list(row) for row in zip(*all_records)]
-
         ## Draw graph
-        plt.clf()
+        fig, ax = plt.subplots()
+        canvas = fig.canvas
         x = []
-        for index, date in enumerate(self.active_dates):
-            x.append(str(self.active_dates[index]).replace("-","/")[2:])
+
+        for index, date in enumerate(self.recorded_dates):
+            x.append(str(self.recorded_dates[index]).replace("-","/")[2:])
+        
+        bottom_values = [0] * len(x)  # Initialize the bottom values for stacking the bars
 
         for y in records_by_cat:
-            plt.bar(x, y)
+            ax.bar(x, y, bottom=bottom_values)  # Stack the bars from the bottom values
+            bottom_values = [b + h for b, h in zip(bottom_values, y)]
 
         plt.legend(self.targets)
-        plt.title("")
 
         if len(dates) > 10:
             plt.xticks(np.arange(0, len(dates), len(dates)//10))
@@ -127,9 +123,5 @@ class Graph:
         plt.xticks(rotation='vertical')
         plt.tight_layout()
 
-        buf = io.BytesIO()
-        plt.savefig(buf)
-
-        buf.seek(0)
-        self.app.root.get_screen("graph").ids.img2.texture = CoreImage(buf, ext="png").texture
-        del buf
+        canvas = FigureCanvasKivyAgg(plt.gcf())
+        self.app.root.get_screen("graph").ids.bx2.add_widget(canvas)
